2fe81a16a182ea98151ae3313f57e234343acc5a
Simon Hubbard <simonjameshubbard@gmail.com>
2023-03-01 15:04:06 +0000

    Updated header in steadyfluxes.py

--- biota/steadyfluxes.py
+++ biota/steadyfluxes.py
@@ -1,20 +1,3 @@
- # 
- # This file is part of the biotechan distribution (https://github.com/xxxx or http://xxx.github.io).
- # Copyright (c) 2023 Upstream Applied Science Ltd.
- # 
- # This program is free software: you can redistribute it and/or modify  
- # it under the terms of the GNU General Public License as published by  
- # the Free Software Foundation, version 3.
- #
- # This program is distributed in the hope that it will be useful, but 
- # WITHOUT ANY WARRANTY; without even the implied warranty of 
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- # General Public License for more details.
- #
- # You should have received a copy of the GNU General Public License 
- # along with this program. If not, see <http://www.gnu.org/licenses/>.
- #
- 
 """
 This module provides performance modelling and yield prediction funcionality
 primarily for cultivated meat technical analaysis, but can easily be adapted 
